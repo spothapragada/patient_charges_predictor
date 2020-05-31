791d75574cee77bc19462b7394f34fc606168894
seethap <seetha.pothapragada@gmail.com>
2020-05-31 00:11:42 -0400

    [add] comments to py file

--- src/test_seetha.py
+++ src/test_seetha.py
@@ -2,12 +2,15 @@ from pycaret.datasets import get_data
 from pycaret.regression import *
 data = get_data('insurance')
 
+#basic setup
 r2 = setup(data, target='charges', session_id=123,
            normalize=True,
            polynomial_features=True, trigonometry_features=True,
            feature_interaction=True,
            bin_numeric_features=['age', 'bmi'])
 
+# create xgb regr model
 xgb = create_model('xgboost')
 
+# save model pickle
 save_model(xgb, '../models/20200530_insurance_xgbreg_deployment_seetha')
